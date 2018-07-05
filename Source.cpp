011587ad34dc7386c6c672ff5b44fe1b73577ea3
nganltp <16520792@gm.uit.edu.vn>
2018-07-05 09:58:55 +0700

    save output Img to Output folder

--- Source.cpp
+++ Source.cpp
@@ -36,7 +36,7 @@ int main(int argc, char **argv)
 		}
 	}
 
-	bool iSuccess = imwrite("E:/intern/Processing - Image - task/Output/output2.png", newImg);
+	bool iSuccess = imwrite("E:/intern/Processing-Image-task/Output/output2.png", newImg);
 	if (!iSuccess)
 		cout << " can't save Image!!!";
 	else cout << " Success!";
